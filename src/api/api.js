bf191d48e2a9f856770021a020a23f188e1ba167
Jack <jackmayer21@gmail.com>
2021-07-28 00:59:14 -0700

    fixing

--- src/api/api.js
+++ src/api/api.js
@@ -4,10 +4,6 @@ const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
 
 /** API Class.
  *
- * Static class tying together methods used to get/send to to the API.
- * There shouldn't be any frontend-specific stuff here, and there shouldn't
- * be any API-aware stuff elsewhere in the frontend.
- *
  */
 
 class BackendApi {
@@ -17,8 +13,6 @@ class BackendApi {
   static async request(endpoint, data = {}, method = "get") {
     console.debug("API Call:", endpoint, data, method);
 
-    //there are multiple ways to pass an authorization token, this is how you pass it in the header.
-    //this has been provided to show you another way to pass the token. you are only expected to read this code for this project.
     const url = `${BASE_URL}/${endpoint}`;
     const params = method === "get" ? data : {};
 
@@ -44,6 +38,5 @@ class BackendApi {
     return res;
   }
 }
-
 
 export default BackendApi;
